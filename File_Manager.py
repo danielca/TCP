97ad7872092e5dd11a6a4f492bd98d0e58fd6989
caseydaniel <bobruels44@hotmail.com>
2014-05-20 12:16:13 -0600

    Files are now combined, version 1.0.0, tested and will be pushed to the server

--- File_Manager.py
+++ File_Manager.py
@@ -1,7 +1,8 @@
+#! /usr/bin/env python
 """
 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
  File Manager Script
- Version: 0.0.2
+ Version: 1.0.0
 
  Created by: Casey Daniel
  Date: 13/05/2014
@@ -13,16 +14,21 @@
 
  Changelog:
     0.0.1:
-     -N/A
+      -N/A
     0.0.2:
-     -Chunk files are now moved out of the main directory into a chunk directory
+      -Chunk files are now moved out of the main directory into a chunk directory
+    0.0.3:
+      -Combined loggers between the unpacking routine and the rest of the file
+    1.0.0:
+      -Script now combines into a master data file, in the same binary format at as the chucnk files
+      -Tested, candidate to start on server
 
 
  TODO:
    -send data to RTEMP
-   -create thumbmail and send to RTEMP
-   -combine files into one larger file
-   -un-comment the timer function
+   -Add MatLab script
+   -create thumbnail and send to RTEMP
+   -un-comment the timer function, commented for testing purposes
 
 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 """
@@ -43,10 +49,12 @@ import shutil
 TIME_DELAY = 60.0 #Seconds
 
 #File Paths
-#RAW_FILE_PATH = "/data/vlf" #Sever Root Path
-RAW_FILE_PATH = "/Users/Casey/Desktop/AboveTest/AboveData/" #Test path for Casey's Mac
-CHUNK_DATA_PATH = "/Users/Casey/Desktop/AboveTest/Data/Chunks"
-FULL_DATA_PATH = "/Users/Casey/Desktop/AboveTest/Data/FullFiles"
+RAW_FILE_PATH = "/data/vlf" #Sever Root Path
+#RAW_FILE_PATH = "/Users/Casey/Desktop/AboveTest/AboveData/" #Test path for Casey's Mac
+#CHUNK_DATA_PATH = "/Users/Casey/Desktop/AboveTest/Data/Chunks" #Test Path
+CHUNK_DATA_PATH = "/data/vlf_chunks" #server path
+#FULL_DATA_PATH = "/Users/Casey/Desktop/AboveTest/Data/FullFiles" #Test Path
+FULL_DATA_PATH = "/data/vlf_full_files" #server path
 
 
 # logging strings
@@ -82,20 +90,22 @@ def loggerInit():
 
 
 def unpackFile(path, fileName):
+    global logger
+
     header = ""
     startKey = ""
     dataList = []
     chan1 = []
     chan2 = []
     found = False
-    logging.info("Starting file " + fileName)
+    logger.info("Starting file " + fileName)
     if not os.path.isfile(os.path.join(path, fileName)):
-        logging.warning("could not find " + fileName)
+        logger.warning("could not find " + fileName)
         return None, None, None
 
     filesize = os.path.getsize(os.path.join(path, fileName))
     if filesize < 1000:
-        logging.warning(fileName + " is below 1000 bytes")
+        logger.warning(fileName + " is below 1000 bytes")
         #contents.close()
         contents = None
         return None, None, None
@@ -116,27 +126,27 @@ def unpackFile(path, fileName):
                 #print("found the }")
                 found = True
         if startKey == "Data_Start":
-            logging.info("Found start key for file "+fileName)
+            logger.info("Found start key for file "+fileName)
         else:
-            logging.warning("No start key found " + fileName + " is corrupted")
+            logger.warning("No start key found " + fileName + " is corrupted")
             contents.close()
             return None, None, None
         #Looks for the end key in the file
         try:
-            logging.debug("Reading the data")
+            logger.debug("Reading the data")
             data = contents.read(40000)
             #endKey = data[len(data)-10:len(data)]
             endKey = contents.read()
         except IOError:
-            logging.warning("IOE error trying to read the end key")
+            logger.warning("IOE error trying to read the end key")
             endKey=""
             contents.close()
             return None, None, None
 
     if endKey == "Data_Stop ":
-        logging.debug("Found end key " )
+        logger.debug("Found end key " )
     else:
-        logging.debug("No end key found in" + fileName)
+        logger.debug("No end key found in" + fileName)
     #Unpacks the data from binary into signed ints
     for i in range(0, len(data), 2):
         value = data[i:i+2]
@@ -146,7 +156,7 @@ def unpackFile(path, fileName):
             dataList.append(number[0])
         else:
             break
-    logging.debug("total points found is " + str(len(dataList)))
+    logger.debug("total points found is " + str(len(dataList)))
     #Splits data into two channels
     for j in range(0, len(dataList)):
         if j % 2 != 0:
@@ -157,11 +167,11 @@ def unpackFile(path, fileName):
             chan1.append(dataList[j])
     #Checks to make sure both channels contain 10000 data points. If this is not true the file is curppted
     if len(chan2) != 10000:
-        logging.warning("Chanel 2 did not containg the right number of data points, " + fileName + " is corupted")
+        logger.warning("Chanel 2 did not containg the right number of data points, " + fileName + " is corupted")
         contents.close()
         return None, None, None
     if len(chan1) != 10000:
-        logging.warning("Chanel 1 did not containg the right number of data points, " + fileName + " is corupted")
+        logger.warning("Chanel 1 did not containg the right number of data points, " + fileName + " is corupted")
         contents.close()
         return None, None, None
     contents.close()
@@ -171,7 +181,25 @@ def unpackFile(path, fileName):
     return header_parts, chan1, chan2
 
 
+def fileCombination(Chan1, Chan2, Header, fileName, filePath):
+    global logger
+
+    Data = []
+    logger.info("combining data into one data set")
+    for i in range(0, len(Chan1)):
+        Data.append(Chan1[i])
+        Data.append(Chan2[i])
+    logger.info("writing single data file at %s" % (str(os.path.join(filePath, fileName))))
+    with open(os.path.join(filePath, fileName), 'wb+') as combinedFile:
+        combinedFile.write("%s%s" % (Header, "Data_Start"))
+        for i in range(0, len(Data)):
+            combinedFile.write(struct.pack('>h',  Data[i]))
+        combinedFile.write("Data_End")
+    logger.info("Done writing %s" % fileName)
+
+
 def cleanUp():
+    global logger
     logger.info("Starting clean up")
     print "starting"
     print RAW_FILE_PATH
@@ -185,10 +213,17 @@ def cleanUp():
         for f in glob.glob("*_00.chunk.dat"):
             #print "found file " + str(f)
             f = f.split("_")
-            file_name = "%s_%s_%s_%s_%s_*" % (f[0], f[1], f[2], f[3], f[4])
-            curuptedData = True
-            for chunk in glob.glob(file_name):
-                while not curuptedData:
+            file_name = "%s_%s_%s_%s_%s_" % (f[0], f[1], f[2], f[3], f[4])
+            search_file = "%s%s" % (file_name, "*")
+            curuptedData = False
+
+            splitPaths = paths.split(("/"))
+            hourDir = "%s/%s/%s" % (splitPaths[-3], splitPaths[-2], splitPaths[-1])
+            chunkDir = os.path.join(CHUNK_DATA_PATH, hourDir)
+
+            for chunk in glob.glob(search_file):
+                print chunk
+                if not curuptedData:
                     logger.info("Unpacking file %s" % (str(chunk)))
                     header, data1, data2 = unpackFile(paths, chunk)
                     if header is not None:
@@ -198,30 +233,30 @@ def cleanUp():
                             Chan2.append(data2[i])
                     else:
                         logger.warning("Invalid file %s" % (str(chunk)))
-                        curuptedData= True
-                splitPaths = paths.split(("/"))
-                hourDir = "%s/%s/%s" % (splitPaths[-3], splitPaths[-2], splitPaths("/")[-1])
-                chunkDir = os.path.join(CHUNK_DATA_PATH, hourDir)
+                        curuptedData = True
                 logger.info("moving file %s to %s" % (str(chunk), chunkDir))
                 if not os.path.isdir(chunkDir):
                     os.makedirs(chunkDir)
                 shutil.move(os.path.join(paths, chunk), os.path.join(chunkDir, chunk))
 
+            Full_File_Name = "%s%s" % (file_name, "full_file.dat")
+            Full_File_Path = os.path.join(FULL_DATA_PATH, hourDir)
 
-
-
-
+            if not os.path.isdir(Full_File_Path):
+                os.makedirs(Full_File_Path)
+            logger.info("Creating combined file")
+            fileCombination(Chan1, Chan2, Headers[0], Full_File_Name, Full_File_Path)
 
 
     #Start this part for recursive checking, disabled for checking
-    #threading.Timer(TIME_DELAY, cleanUp).start()
+    threading.Timer(TIME_DELAY, cleanUp).start()
 
 
 def main():
 
     global logger
 
-    logger.info("Starting clean up")
+    logger.info("Starting initial clean up")
 
     cleanUp()
 
