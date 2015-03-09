f12ecdd58da7fd353af7e67f63dfe9501e30eb39
caseydaniel <bobruels44@hotmail.com>
2015-03-09 14:20:14 -0600

    Polished up sd_readder.cpp a bit. Also added command line arguments for the input file and out directory. 2 optional arguments. -hf for the header fix, and -sd for the sd size.

--- sdCard/sd_reader.cpp
+++ sdCard/sd_reader.cpp
@@ -2,17 +2,40 @@
 #include <iostream>
 #include <math.h>
 
+/*
+    sd_reader.pp
+    Created By: Casey Daniel
+    Date:March 2015
+
+    This script reads the contents of the ABOVE SD cards, gathers individual data files, and places them in the specified
+    directory following the directory format and file names used by ABOVE.
+
+    There are 2 required input args from the command line, input file and out directory. These can be entered as
+    ./sdCard if=Inputfile of=OutputDirecotry
+    or
+    ./sdCard Inputfile OutputDirectory
+
+    There are also 2 optional arguments.
+    -hf to fix the header information if it is known that some fields are incorect, as in the first 2 rounds of SD cards
+    -sd SDSIZE to change the sd card size from the defaule 128GB.
+ */
+
 using namespace std;
 
-//string dataLocation = "/dev/rdisk2";  // Variable for the mounting point of the SD Card.
-string dataLocation = "/Volumes/Untitled/data/smith_data_backup.dat";  // Test location
+//Constants
+string dataLocation;
+string outDir;
 unsigned long long int sectorSize = 512;
 unsigned long long int bytesRead = 0;
 long long int sdSize = 128000000000;
-string outDir = "/Volumes/Untitled/data/readData/";
 char data_stop[10] = "Data_Stop";
+int headerFix = 0;
 
 char* fixHeader(char* header) {
+    /*
+    Some of the header contents written on the eariler SD cards were incorect. Mainly the file size, and number of files
+    This functions returns the header string with fixed contets. Brute force was the only way here.
+     */
     string date = "";
     string time = "";
     string site = "";
@@ -114,10 +137,6 @@ char* fixHeader(char* header) {
             sd += header[i];
         }
 
-
-
-
-
         //paranoia of infinite loops
         if (i > 350) {
             break;
@@ -134,6 +153,7 @@ char* fixHeader(char* header) {
 
 
 char* findHeader(char* inputData) {
+    //Finds the end of the header, and returns the header contents
     int i = 1;
 
     //look for the end of the header
@@ -161,6 +181,10 @@ char* findHeader(char* inputData) {
 }
 
 void WriteData(std::ifstream& file_stream, char* data) {
+    /*
+      fuction to handle the writing of the directory. Takes the file stream object, as well as any current data from the
+      sector
+     */
     string date = "";
     string time = "";
     string site = "";
@@ -168,8 +192,12 @@ void WriteData(std::ifstream& file_stream, char* data) {
     string fileNo = "";
     string fileSize = "";
 
-    char* header1 = findHeader(data);
-    char* header = fixHeader(header1); // This function is used to fix the header values, most of all the file size.
+    //Extract the header from the start
+    char* header = findHeader(data);
+    //if the header fix argument was given, fix the header portions
+    if (headerFix == 1) {
+        header = fixHeader(header); // This function is used to fix the header values, most of all the file size.
+    }
 
     //loop over the header to find the date, time, site ID, Instrument ID, and expected file size.
     int commasFound = 0;
@@ -236,6 +264,7 @@ void WriteData(std::ifstream& file_stream, char* data) {
     file_stream.read(binaryData, fileSizeInt);
     bytesRead += fileSizeInt;
 
+    //update the site names if needed. If more changes are made, place them here
     if (site == "piwa") {
         site = "pina";
     } else if (site == "smth") {
@@ -252,8 +281,6 @@ void WriteData(std::ifstream& file_stream, char* data) {
     string mkdirCommand = "mkdir -p " + dateDir;
     system(mkdirCommand.c_str());
 
-    //cout << "size " << sizeof(binaryData) << '\n';
-
     //write data to file
     try {
         //cout << "Write the data file " << '\n';
@@ -269,43 +296,96 @@ void WriteData(std::ifstream& file_stream, char* data) {
         cout << "Unable to write the file " << fileName << '\n';
     }
 
-    //read till the start of the next sector
+    //read till the start of the next sector, and de-malloc the data
     unsigned long long int nextSector = (unsigned long long int) ceil(((double) bytesRead)/(double) sectorSize);
     unsigned long long int startOfNextSector;
     startOfNextSector = nextSector * 512;
-    //cout << startOfNextSector << '\n';
     unsigned long long int bytesToRead =  (startOfNextSector - bytesRead);
     char dump[bytesToRead];
     file_stream.read(dump, (int) bytesToRead);
     bytesRead += bytesToRead;
     free(binaryData);
 }
+//Main Entry Point
+int main(int argc, char* argv[]) {
 
-int main() {
+    //Check the number of input args. Should be minimum 2 (input file and out directory) plus 3 more optional arguments
+    //for the header fix and sd size.
+    if (argc < 3  || argc > 6) {
+        cout << "incorrect paramters inputed. Please restart with the input and output files";
+        return 1;
+    }
 
+    //Loops through the input args, and checks to see if its an input file or out dir, or the argument for the header fix
+    //If not explicitly set using id= or od=, it will assume the format ./sdCard inputFile outDir
+    //the file and/or directory can also be in the middle of quotations.
+    for (int k = 1; k < argc; k++) {
+        string inputArg = std::string(argv[k]);
+        if (inputArg.substr(0, 3) == "if=") {
+            dataLocation = inputArg.substr(3, inputArg.length());
+            cout << "Pulling data file " << dataLocation << '\n';
+
+        } else if (inputArg.substr(0, 4) == "if='" || inputArg.substr(0, 4) == "if=\"") {
+            dataLocation = inputArg.substr(3, inputArg.length() - 1);
+            cout << "Pulling data file " << dataLocation << '\n';
+
+        } else if (inputArg.substr(0, 3) == "od=") {
+            outDir = inputArg.substr(3, inputArg.length());
+            cout << "putting data in the directory " << outDir << '\n';
+
+        } else if (inputArg.substr(0, 4) == "od='" || inputArg.substr(0, 4) == "od=\"") {
+            outDir = inputArg.substr(3, inputArg.length() - 1);
+            cout << "putting data in the directory " << outDir << '\n';
+
+        } else if (inputArg == "-hf") {
+            headerFix = 1;
+            cout << "Will fix the header" << '\n';
+
+        } else if (inputArg == "-sd") {
+            k++; //increment k to get the file size
+            sdSize = (long long) atol(argv[k]);
+            cout << "SD size of " << sdSize << '\n';
+
+        } else if (k == 1) {
+            if (inputArg.substr(0,1) == "'" || inputArg.substr(0,1) == "\"") {
+                dataLocation = inputArg.substr(1,inputArg.length()-1);
+            } else {
+                dataLocation = inputArg;
+            }
+            cout << "Pulling data file " << dataLocation << '\n';
+
+        } else if (k == 2) {
+            if (inputArg.substr(0,1) == "'" || inputArg.substr(0,1) == "\"") {
+                outDir = inputArg.substr(1,inputArg.length()-1);
+            } else {
+                outDir = inputArg;
+            }
+            cout << "putting data in the directory " << outDir << '\n';
+        }
+    }
+
+    //check to make sure there not null
+    if (outDir == NULL || dataLocation == NULL) {
+        cout << "Could not find a valid input and/or out directory. Please see the documentation to correctly format this input";
+        return 1;
+    }
+
+    //open the data file
     ifstream file_stream;
     file_stream.open(dataLocation);
 
-    //file_stream.seekg(512000000); // TESTING ONLY!!!!!!!!!!!!
-
-
+    //Large number of loops, didn't want to do an infinite one because paranoia
     for( int j = 0; j < 250000000; j++ ) {
-
-
-        if (j >= 3){
-           // cout << "test" << '\n';
-        }
-
-        char* data = (char*) malloc((size_t) sectorSize); // figure out a decent size for this.....
-
+        //malloc a sector
+        char* data = (char*) malloc((size_t) sectorSize);
 
         try {
 
             // read the data file... this will be fun
-            //cout << "Reading the data file" << '\n';
             file_stream.read(data, (int) sectorSize);
             bytesRead += sectorSize;
 
+            //check for the start of a secotr
             if (data[0] == '{') {
                 WriteData(file_stream, data);
 
@@ -315,7 +395,7 @@ int main() {
                 continue;
 
             } else {
-                //cout << "no start here....." << bytesRead << '\n';
+                //Can't read more than whats in the SD card
                 if (bytesRead > sdSize) {
                     break;
                 }
@@ -326,7 +406,7 @@ int main() {
             cout << "Error" << "\n";
             break;
         }
-
+        //de-malloc the sectors
         free(data);
 
     }
